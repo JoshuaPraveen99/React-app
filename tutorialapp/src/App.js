d4302f653c11dad17cdd84abc37e5bd80d3009e2
JoshuaPraveen99 <65905555+JoshuaPraveen99@users.noreply.github.com>
2023-09-25 22:03:33 +0530

    react app created

---
+++ tutorialapp/src/App.js
@@ -0,0 +1,72 @@
+import logo from './logo.svg';
+import pic1 from "./pic1.avif";
+import pic2 from "./pic2.jpg";
+import pic3 from "./pic3.avif";
+import pic4 from "./pic4.avif";
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+function UserProfile({ user }) {
+  const [showEmail, setShowEmail] = useState(false);
+
+  const toggleEmail = () => {
+    setShowEmail(!showEmail);
+  };
+
+  return (
+    <div className="user-profile">
+      <img src={user.profilePicture} alt={`${user.name}'s Profile`} />
+      <h2>{user.name}</h2>
+      <button onClick={toggleEmail}>Email</button>
+      {showEmail && <p>Email: {user.email}</p>}
+    </div>
+  );
+}
+
+function UserList({ users }) {
+  return (
+    <div className="user-list">
+      {users.map((user, index) => (
+        <UserProfile key={index} user={user} />
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  const usersData = [
+    {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      profilePicture: pic1,
+    },
+    {
+      name: 'Jane Smith',
+      email: 'janesmith@example.com',
+      profilePicture: pic2,
+    },
+
+    {
+      name: 'Joshua',
+      email: 'joshua@example.com',
+      profilePicture: pic3,
+    },
+
+    {
+      name: 'Imran',
+      email: 'imran@example.com',
+      profilePicture:pic4,
+    }
+    // Add more user data as needed
+  ];
+  return (
+    <div className="App">
+      <h1>User Profiles</h1>
+      <UserList users={usersData} />
+    </div>
+  );
+}
+
+export default App;
